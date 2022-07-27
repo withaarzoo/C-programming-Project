d3fc2741e039e298d85596c73602f035bc1d00c6
Rumi Khatun <110076050+RUMI20@users.noreply.github.com>
2022-07-27 08:08:23 +0530

    printf function
    i have change output

--- Unit Conversion.c
+++ Unit Conversion.c
@@ -19,20 +19,20 @@ int main()
     while (b == 1)
     {
 
-        printf("press 1 if you want to convert km to mile\nPress 2 if you want to convert inch to feet\nPress 3 if you want to convert cm to inch\nPress 4 if you want to convert pount to kg\nPress 5 if you want to convert inch to meter\nPress 6 if you want to convert celsius to fahrenheit\n\n");
+        printf("Press 1 if you want to convert km to mile\nPress 2 if you want to convert inch to feet\nPress 3 if you want to convert cm to inch\nPress 4 if you want to convert pount to kg\nPress 5 if you want to convert inch to meter\nPress 6 if you want to convert celsius to fahrenheit\n\n");
         scanf("%d", &a);
         printf("\n");
 
         if (a == 1)
         {
-            printf("enter your km value : ");
+            printf("Enter your km value : ");
             scanf("%d", &c);
 
             printf("%d km = %.2f mile\n\n", c, c / 1.609344);
         }
         else if (a == 2)
         {
-            printf("enter your inch value : ");
+            printf("Enter your inch value : ");
             scanf("%f", &d);
 
             float feet = d / 12;
@@ -40,28 +40,28 @@ int main()
         }
         else if (a == 3)
         {
-            printf("enter your cm value : ");
+            printf("Enter your cm value : ");
             scanf("%d", &e);
 
             printf("%d cm = %.2f inch\n\n", e, e / 2.54);
         }
         else if (a == 4)
         {
-            printf("enter your pound value : ");
+            printf("Enter your pound value : ");
             scanf("%d", &f);
 
             printf("%d pound = %.2f kg\n\n", f, f * 0.45359237);
         }
         else if (a == 5)
         {
-            printf("enter your inch value : ");
+            printf("Enter your inch value : ");
             scanf("%d", &g);
 
             printf("%d inch = %.2f meter\n\n", g, g * 0.025);
         }
         else if (a==6)
         {
-            printf("enter your celsius value : ");
+            printf("Enter your celsius value : ");
             scanf("%d", &h);
 
             printf("%d celsius = %d fahrenheit\n\n", h, (h*9/5)+32);
@@ -70,7 +70,7 @@ int main()
 
         else
         {
-            printf("please check your input value\n\n");
+            printf("Please check your input value\n\n");
         }
     }
 }
